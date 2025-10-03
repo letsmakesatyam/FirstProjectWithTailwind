81086e43f13b6a5995fdf66a0205be17202fe4c6
letsmakesatyam <satyamrevgade2@gmail.com>
2025-10-03 07:12:24 +0000

    create components in component folder

--- tailwindcss/src/App.jsx
+++ tailwindcss/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 
 import './App.css'
 
@@ -6,9 +6,19 @@ function App() {
 
 
   return (
-    <>
-    <h1 className='text-9xl'>Hello</h1>
-    </>
+    <div class="relative w-full h-screen overflow-hidden bg-[#0f021f]">
+  <div class="absolute inset-0 opacity-20">
+    <div class="absolute inset-0 bg-[linear-gradient(to_right,#9333ea_1px,transparent_1px),linear-gradient(to_bottom,#9333ea_1px,transparent_1px)] bg-[size:80px_80px] animate-move"></div>
+  </div>
+
+  <div class="absolute w-[700px] h-[700px] bg-violet-700 rounded-full mix-blend-screen filter blur-3xl opacity-30 animate-blob"></div>
+  <div class="absolute w-[600px] h-[600px] bg-fuchsia-600 rounded-full mix-blend-screen filter blur-3xl opacity-30 animate-blob animation-delay-2000"></div>
+  <div class="absolute w-[500px] h-[500px] bg-indigo-700 rounded-full mix-blend-screen filter blur-3xl opacity-30 animate-blob animation-delay-4000"></div>
+
+  <div class="relative z-10 flex items-center justify-center h-full">
+    <h1 class="text-5xl font-bold text-violet-100 drop-shadow-2xl">Welcome to My Future 🚀</h1>
+  </div>
+</div>
   )
 }
 
---
+++ tailwindcss/src/components/Experience/Experience.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { experiences } from "../../constants";
+const Experience = ()=>{
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+export default Experience
